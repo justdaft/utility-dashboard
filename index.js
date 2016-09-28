08a5f75fc770ac6df79b6eaa571e17fdef0e08cc
billywestbury <billy.westbury@outlook.com>
2016-09-28 20:02:31 +0100

    six commit

--- index.js
+++ index.js
@@ -3,75 +3,119 @@
 var blessed = require('blessed')
 var contrib = require('blessed-contrib')
 var scan = require('./scan');
-
-
-
 var screen = blessed.screen()
 var fs = require('fs');
+var os = require('os');
 var username = require('username');
 var uname = ''
+
 username().then(usern => {
-    // console.log(username);
-    uname = usern;;
-    //=> 'sindresorhus' 
+  uname = usern;
 });
 uname = username.sync();
 
-// var list = blessed.list({
-//   parent: screen,
-//   label: ' {bold}{cyan-fg}Art List{/cyan-fg}{/bold} (Drag Me) ',
-//   tags: true,
-//   draggable: true,
-//   top: 0,
-//   right: 0,
-//   height: '50%',
-//   keys: true,
-//   vi: true,
-//   mouse: true,
-//   border: 'line'})
-
-
 var grid = new contrib.grid({ rows: 12, cols: 12, screen: screen })
-
-//grid.set(row, col, rowSpan, colSpan, obj, opts)
-// var map = grid.set(1, 1, 6, 6, contrib.map, {label: 'World Map'})
-var box1 = grid.set(0, 0, 5, 5, blessed.box, { label: 'Curent User', content: 'Username: ' + uname })
+var box1 = grid.set(0, 0, 5, 5, blessed.box, { label: 'Curent User' })
 var box2 = grid.set(5, 0, 5, 5, blessed.box, { content: 'My Box2' })
 var box3 = grid.set(0, 5, 5, 5, blessed.box, { content: 'My Box3' })
 var box4 = grid.set(5, 5, 5, 5, blessed.box, { content: 'My Box4' })
-var box5 = grid.set(10, 0, 2, 12, blessed.box, { label: 'Logs', content:'Directory: ' + __dirname + '/logs' })
+var box5 = grid.set(10, 0, 2, 12, blessed.box, { label: 'Logs', content: 'Directory: ' + __dirname + '/logs' })
 var box6 = grid.set(0, 10, 10, 2, blessed.box, { content: 'My Box6' })
 
-var log = grid.set(10, 0, 2, 12, contrib.log, 
-  { fg: "green"
-  , selectedFg: "green"
-  , label: 'Server Log'})
+var log = grid.set(10, 0, 2, 12, contrib.log,
+  {
+    fg: "green"
+    , selectedFg: "green"
+    , label: 'Server Log'
+  })
 
-var table =  grid.set(10, 0, 2, 12, contrib.table, 
-  { keys: true
-  , fg: 'green'
-  , label: 'Active Processes'
-  , columnSpacing: 1
-  , columnWidth: [24, 10, 10]})
+var userDataTable = grid.set(0, 0, 5, 5, contrib.table,
+  {
+    keys: true
+    , fg: 'white'
+    , selectedFg: 'white'
+    , selectedBg: 'blue'
+    , interactive: false
+    , label: 'Files'
+    , width: '30%'
+    , height: '30%'
+    , border: { type: "line", fg: "cyan" }
+    , columnSpacing: 10
+    , columnWidth: [100]
+  })
 
-var tree = scan('.', 'files');
-
-// table.setData(data: tree})
+var table = grid.set(5, 0, 5, 5, contrib.table,
+  {
+    keys: true
+    , fg: 'white'
+    , selectedFg: 'white'
+    , selectedBg: 'blue'
+    , interactive: false
+    , label: 'Files'
+    , width: '30%'
+    , height: '30%'
+    , border: { type: "line", fg: "cyan" }
+    , columnSpacing: 10
+    , columnWidth: [12]
+  })
 
 screen.render()
-// box2.content = blessed.list();
+
+function generateUserDateTable() {
+
+  var uName = 'Name:' + uname;
+  var hostName = 'Hostname: ' + os.hostname();
+  var osPlatform = 'Platform: ' + os.platform();
+  var osTotalMem = 'Total Memory: ' + os.totalmem();
+  var osUptime = 'Uptime: ' + os.uptime();
+  var userHomeDir = 'Home Dir: ' + os.homedir();
+  var osLoadAvg = 'CPU Average Load: ' + os.loadavg();
+
+  userDataTable.setData(
+    {
+      headers: ['']
+      , data: [
+        [uName],
+        [hostName],
+        [osPlatform],
+        [osTotalMem],
+        [osUptime],
+        [userHomeDir],
+        [osLoadAvg]
+      ]
+    })
+}
+
+function generateTable() {
+  var data = []
+  var tree = scan('.', 'files');
+  for (var i = 0; i < tree.items.length; i++) {
+    var row = []
+    row.push(tree.items[i].name)
+    data.push(row)
+  }
+  table.setData(
+    {
+      headers: ['']
+      , data: data
+    })
+}
+
+generateTable();
+generateUserDateTable()
 
 
+table.focus()
+screen.append(table)
 
-console.log(tree);
+setInterval(function () {
+  generateTable()
+  screen.render()
+}, 500)
 
-// setInterval(function () {
-//     // generateTable()
-//     screen.render()
-// }, 500)
-
-  screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-     return process.exit(0);
-   });
+screen.render()
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+  return process.exit(0);
+});
 
 
